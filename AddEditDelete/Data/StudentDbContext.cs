bace0ef3905e1bbfac8e54e47c859801038fa792
maitiger <allthing.mmo@gmail.com>
2018-11-20 18:37:05 +0700

    NetCoreMVC

---
+++ AddEditDelete/Data/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddEditDelete.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext (DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AddEditDelete.Models.Student> Student { get; set; }
+    }
+}
